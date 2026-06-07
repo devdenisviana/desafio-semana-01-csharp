c784d0e249a5e1edf40868f197267de5fd7219f3
Denis Viana <41395264+devdenisviana@users.noreply.github.com>
2026-06-07 07:09:12 -0300

    Exercicio 2 - Cadastro de Produto

---
+++ Exercicio2-CadastroProduto/Program.cs
@@ -0,0 +1,7 @@
+﻿string nomeProduto = "Teclado Mecânico";
+decimal preco = 250.00m;
+int quantidade = 15;
+
+Console.WriteLine("Produto " + nomeProduto);
+Console.WriteLine("Preço: R$ " + preco.ToString("F2")); //ToString converte o decimal para string para juntar com o Preço: R$ e o valor. F2 é fixed-point, com 2 casas decimais.
+Console.WriteLine("Quantidade: " + quantidade);
