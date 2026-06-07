c7fa4f6415d8e33f27ecf2f631311a3506d5fd30
Denis Viana <41395264+devdenisviana@users.noreply.github.com>
2026-06-07 07:17:51 -0300

    Exercicio 3 - Calculadora Simples

---
+++ Exercicio3-CalculadoraSimples/Program.cs
@@ -0,0 +1,12 @@
+﻿int numero1 = 10;
+int numero2 = 5;
+
+int soma = numero1 + numero2;
+int subtracao = numero1 - numero2;
+int multiplicacao = numero1 * numero2;
+int divisao = numero1 / numero2; ;
+
+Console.WriteLine(numero1 + " + " + numero2 + " = " + soma);
+Console.WriteLine(numero1 + " - " + numero2 + " = " + subtracao);
+Console.WriteLine(numero1 + " * " + numero2 + " = " + multiplicacao);
+Console.WriteLine(numero1 + " / " + numero2 + " = " + divisao);
