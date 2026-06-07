6765bf911a9641a6c71e2c40baa551b4265d334a
Denis Viana <41395264+devdenisviana@users.noreply.github.com>
2026-06-07 07:01:24 -0300

    Exercicio 1 - Apresentacao Pessoal

--- Exercicio1-ApresentacaoPessoal/Program.cs
+++ Exercicio1-ApresentacaoPessoal/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string nome = "Denis";
+int idade = 46;
+string cidade = "Fortaleza";
+string profissaoDosSonhos = "Desenvolvedor Backend";
+
+Console.WriteLine("Nome: " + nome);
+Console.WriteLine("Idade " + idade);
+Console.WriteLine("Cidade " + cidade);
+Console.WriteLine("Profissão dos sonhos " + profissaoDosSonhos);
