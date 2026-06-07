c7a08981809f8544e74d833b64fbd70e4d6fbf98
Denis Viana <41395264+devdenisviana@users.noreply.github.com>
2026-06-07 07:48:48 -0300

    Desafio Bonus - Funcao CalcularMedia

--- Exercicio5-PrimeiraFuncao/Program.cs
+++ Exercicio5-PrimeiraFuncao/Program.cs
@@ -1,6 +1,18 @@
 ﻿ExibirMensagem();
 
+double media = CalcularMedia(8, 7, 9);
+
+Console.WriteLine("Nota 1: 8");
+Console.WriteLine("Nota 2: 7");
+Console.WriteLine("Nota 3: 9");
+Console.WriteLine("Média: " + media);
+
 void ExibirMensagem()
 {
-    Console.WriteLine("Estou aprendendo c# e criando minhas primeiras funções");
+    Console.WriteLine("Estou aprendendo C# e criando minhas primeiras funções!");
+}
+
+double CalcularMedia(double nota1, double nota2, double nota3)
+{
+    return (nota1 + nota2 + nota3) / 3;
 }
